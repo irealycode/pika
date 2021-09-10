ab379429f47aaf0298844c94f4f29485bce0ef7e
irealycode <irealycode@gmail.com>
2021-09-10 02:16:20 +0000

    Add files via upload

---
+++ pika/App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import login_suck from './assets/login';
+import register_suck from './assets/register';
+import suck_upload from './assets/uploadPhotos';
+import suck_Drawer from './assets/Drawer';
+
+const Stack = createNativeStackNavigator();
+export default function App() {
+  
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="login" screenOptions={{headerShown: false}} >
+        <Stack.Screen name="login" component={login_suck} />
+        <Stack.Screen name="register" component={register_suck} />
+        <Stack.Screen name="upload" component={suck_upload} />
+        <Stack.Screen name="drawer" component={suck_Drawer} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
---
+++ pika/assets/home.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+
+export default function suck_Home() {
+  
+  return (
+    <Text>home</Text>
+  );
+}
